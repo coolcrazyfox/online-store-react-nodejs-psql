67c3c24e98f73763e98d100874661d7aa35f44b9
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-09-04 21:38:19 +0300

    changing client

--- client/src/components/AppRouter.js
+++ client/src/components/AppRouter.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import {Route, Routes} from "react-router-dom";
 import {publicRoutes, authRoutes} from "../routes";
+import Error404 from "../pages/Error404";
+import Shop from "../pages/Shop";
+import Auth from "../pages/Auth";
+import Basket from "../pages/Basket";
+import Admin from "../pages/Admin";
+import DevicePage from "../pages/DevicePage";
 export const PATH = {
-    LOGIN: '/login',
-    REGISTRATION: '/register',
-    NEW_PASSWORD: '/set-new-password',
-    FORGOT: '/forgot',
+    LOGIN: '/auth',
+    REGISTRATION: '/auth',
+    ADMIN: '/admin',
     SHOP: '/shop',
-    EDIT_PROFILE: '/edit-profile',
-    PACKS: '/packs',
-    CARDS: '/cards',
-    LEARN: '/learn'
+    BASKET: '/basket',
+    DEVICE: '/device'
+
 }
 
 const AppRouter = () => {
@@ -19,16 +23,12 @@ const AppRouter = () => {
 
         <Routes>
             <Routes>
-                <Route path={'/'} element={<Profile/>}/>
+                <Route path={'/'} element={<Shop/>}/>
+                <Route path={PATH.ADMIN} element={<Admin/>}/>
                 <Route path={PATH.LOGIN} element={<Auth/>}/>
-                <Route path={PATH.REGISTRATION} element={<Registration/>}/>
-                <Route path={PATH.NEW_PASSWORD} element={<NewPassword/>}/>
-                <Route path={PATH.FORGOT} element={<Forgot/>}/>
-                <Route path={PATH.PROFILE} element={<Profile/>}/>
-                <Route path={PATH.EDIT_PROFILE} element={<EditProfile/>}/>
-                <Route path={PATH.PACKS} element={<PackList/>}/>
-                <Route path={PATH.CARDS + '/:packId'} element={<CardsList/>}/>
-                <Route path={PATH.LEARN + '/:packId/:packName/:cardsTotalCount'} element={<Learn/>} />
+                <Route path={PATH.REGISTRATION} element={<Auth/>}/>
+                <Route path={PATH.BASKET} element={<Basket/>}/>
+                <Route path={PATH.DEVICE + '/:id'} element={<DevicePage/>}/>
                 <Route path={'*'} element={<Error404/>}/>
             </Routes>
             {/*{isAuth && authRoutes.map(({path, Component}) =>*/}
