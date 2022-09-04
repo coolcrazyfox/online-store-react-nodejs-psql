9911afe5025c4c3440a868b0b3880197e752568c
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-09-04 21:28:01 +0300

    changing client

--- client/src/components/AppRouter.js
+++ client/src/components/AppRouter.js
@@ -1,10 +1,47 @@
 import React from 'react';
+import {Route, Routes} from "react-router-dom";
+import {publicRoutes, authRoutes} from "../routes";
+export const PATH = {
+    LOGIN: '/login',
+    REGISTRATION: '/register',
+    NEW_PASSWORD: '/set-new-password',
+    FORGOT: '/forgot',
+    SHOP: '/shop',
+    EDIT_PROFILE: '/edit-profile',
+    PACKS: '/packs',
+    CARDS: '/cards',
+    LEARN: '/learn'
+}
 
 const AppRouter = () => {
+    const isAuth = false
     return (
-        <div>
-            AppRouter
-        </div>
+
+        <Routes>
+            <Routes>
+                <Route path={'/'} element={<Profile/>}/>
+                <Route path={PATH.LOGIN} element={<Auth/>}/>
+                <Route path={PATH.REGISTRATION} element={<Registration/>}/>
+                <Route path={PATH.NEW_PASSWORD} element={<NewPassword/>}/>
+                <Route path={PATH.FORGOT} element={<Forgot/>}/>
+                <Route path={PATH.PROFILE} element={<Profile/>}/>
+                <Route path={PATH.EDIT_PROFILE} element={<EditProfile/>}/>
+                <Route path={PATH.PACKS} element={<PackList/>}/>
+                <Route path={PATH.CARDS + '/:packId'} element={<CardsList/>}/>
+                <Route path={PATH.LEARN + '/:packId/:packName/:cardsTotalCount'} element={<Learn/>} />
+                <Route path={'*'} element={<Error404/>}/>
+            </Routes>
+            {/*{isAuth && authRoutes.map(({path, Component}) =>*/}
+            {/*    <Route key={path} path={path} element={Component}/>*/}
+            {/*)*/}
+            {/*}*/}
+
+            {/*{publicRoutes.map(({path, Component}) =>*/}
+            {/*    <Route key={path} path={path} element={Component}/>*/}
+            {/*)*/}
+            {/*}*/}
+
+        </Routes>
     );
 };
 
---
+++ client/src/pages/Error404.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Error404 = () => {
+    return (
+        <div>
+            Page not found
+            ERROR 404
+        </div>
+    );
+};
+
+export default Error404;
